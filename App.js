2bc56df2bfaffe6886eb45a19e71fa82f33d9222
skinksteek <linusigelstrom@gmail.com>
2022-02-28 14:10:27 +0100

    commit

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 
 const App = () => {
   return (
     <View style={styles.body}>
-      <View style={styles.view1}>
-        <Text style={styles.text}>1</Text>
-      </View>
-      <View style={styles.view2}>
-        <Text style={styles.text}>2</Text>
-      </View>
-      <View style={styles.view3}>
-        <Text style={styles.text}>3</Text>
-      </View>
+      <Text style={styles.text}>Hejsan</Text>
     </View>
   );
 };
@@ -20,37 +12,8 @@ const App = () => {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#fe6e46',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
-  view1: {
-    height: 100,
-    width: 100,
-    backgroundColor: '#00ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view2: {
-    height: 100,
-    width: 100,
-    backgroundColor: '#ff00ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view3: {
-    height: 100,
-    width: 100,
-    backgroundColor: '#ffff00',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'black',
-    fontSize: 30,
-    fontStyle: 'italic',
-    textTransform: 'uppercase',
   },
 });
 
