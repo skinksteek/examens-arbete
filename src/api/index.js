81ced34848fc5140365e32d2f14529b54434fbcc
skinksteek <linusigelstrom@gmail.com>
2022-03-04 18:04:57 +0100

    dynamic api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,21 @@
-export const fetchData = async URL => {
-  const response = await fetch(URL);
-  const json = await response.json();
-  return json;
+import access from '../secret/access.json';
+
+export const fetchFunction = async (URL, method, body) => {
+  try {
+    const response = await fetch(URL, {
+      method: method,
+      mode: 'no-cors',
+      headers: {
+        Authorization: `Basic ${access.secret}`,
+      },
+      body: JSON.stringify(body),
+    });
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch (e) {
+    console.log(JSON.stringify(e));
+  }
 };
 
-export default fetchData;
+export default fetchFunction;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,9 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
+import fetchFunction from '../api';
 
 export const HomeScreen = ({navigation}) => {
+  const [data, setData] = useState();
   return (
     <View style={styles.body}>
       <Text>Home Screen</Text>
@@ -9,6 +11,17 @@ export const HomeScreen = ({navigation}) => {
         title="Gå till nyheter"
         onPress={() => navigation.navigate('Nyheter')}
       />
+      <Button
+        title="Fetch"
+        onPress={async () => {
+          const response = await fetchFunction(
+            'http://linus.labb.soleilit.se/rest-api/1/0/3.484d9c7317ce4f0e1b69ed/nodes',
+            'get',
+          );
+          setData(response);
+        }}
+      />
+      <Text>{JSON.stringify(data)}</Text>
     </View>
   );
 };
