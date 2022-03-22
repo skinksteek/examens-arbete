bbe0bd92af2c2338940d5252e6ed3e057ec1c077
skinksteek <linusigelstrom@gmail.com>
2022-03-22 13:51:28 +0100

    moved templates to constants file

---
+++ src/utils/Constants.js
@@ -0,0 +1,15 @@
+import SubPageScreen from '../screens/SubPageScreen';
+import EmptyScreen from '../screens/EmptyScreen';
+import WidePageScreen from '../screens/WidePageScreen';
+import CenteredScreen from '../screens/CenteredScreen';
+import PageListingScreen from '../screens/PageListingScreen';
+
+const templates = {
+  '91.484d9c7317ce4f0e1b68b3': CenteredScreen,
+  '91.484d9c7317ce4f0e1b68ad': SubPageScreen,
+  '91.484d9c7317ce4f0e1b68b8': EmptyScreen,
+  '91.484d9c7317ce4f0e1b6954': WidePageScreen,
+  '91.484d9c7317ce4f0e1b6946': PageListingScreen,
+};
+
+export default templates;
