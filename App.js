67c3c635581bd1a75f5c041a8a733159498cc408
skinksteek <linusigelstrom@gmail.com>
2022-03-22 13:53:42 +0100

    App.js cleanup and packages

--- App.js
+++ App.js
@@ -1,28 +1,11 @@
-import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen';
-import NewsScreen from './src/screens/NewsScreen';
-
-const Stack = createNativeStackNavigator();
-
-const navigationStyles = {
-  headerStyle: {
-    backgroundColor: '#f4511e',
-  },
-  headerTintColor: '#fff',
-  headerTitleStyle: 'bold',
-  headerTitleAlign: 'center',
-};
+import React from 'react';
+import Menu from './src/components/Menu';
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={navigationStyles}>
-        <Stack.Screen name="Hem" component={HomeScreen} />
-        <Stack.Screen name="Nyheter" component={NewsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <Menu />
+    </>
   );
 }
 
