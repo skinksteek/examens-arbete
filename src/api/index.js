b82accc37bdc1911c2217e30e5cd9d3b3476697a
skinksteek <linusigelstrom@gmail.com>
2022-03-22 13:53:20 +0100

    new fetch approach with RNFetchBlob

--- src/api/index.js
+++ src/api/index.js
@@ -1,21 +1,9 @@
-import access from '../secret/access.json';
+import RNFetchBlob from 'rn-fetch-blob';
 
-export const fetchFunction = async (URL, method, body) => {
-  try {
-    const response = await fetch(URL, {
-      method: method,
-      mode: 'no-cors',
-      headers: {
-        Authorization: `Basic ${access.secret}`,
-      },
-      body: JSON.stringify(body),
-    });
-    const json = await response.json();
-    console.log(json);
-    return json;
-  } catch (e) {
-    console.log(JSON.stringify(e));
-  }
+export const fetchFunction = async (url, method, body) => {
+  return RNFetchBlob.config({
+    trusty: true,
+  }).fetch(method, url);
 };
 
 export default fetchFunction;
