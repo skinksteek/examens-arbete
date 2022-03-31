dca80c31b33ae7587bad169cfc087469c482a482
skinksteek <linusigelstrom@gmail.com>
2022-03-31 12:48:09 +0200

    added global styles file

---
+++ src/styles/global.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+import {Colors} from './Colors';
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: Colors.gray,
+  },
+});
