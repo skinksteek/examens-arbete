0808256e192018c8bce33b8778ee08bba8c77752
skinksteek <linusigelstrom@gmail.com>
2022-04-04 09:35:54 +0200

    dynamic button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,32 +1,51 @@
 import React from 'react';
-import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import {View, Text, TouchableOpacity, Dimensions} from 'react-native';
 import Colors from '../styles/Colors';
 
-export const Button = ({text}) => {
-  const navigation = useNavigation();
+const width = Dimensions.get('window').width;
+
+export const Button = ({
+  text,
+  onPress,
+  type = 'filled',
+  bordered = false,
+  size = 'large',
+}) => {
+  const large = width / 1.3;
+  const small = width / 2;
+  const btnSize = size === 'large' ? large : small;
+  const btnBgColor = type === 'filled' ? Colors.orange : Colors.white;
+  const btnTextColor = type === 'filled' ? Colors.black : Colors.orange;
+  const btnBorderRadius = bordered ? 30 : 5;
+
+  const containerStyles = {
+    backgroundColor: btnBgColor,
+    paddingVertical: 8,
+    margin: 10,
+    width: btnSize,
+    borderRadius: btnBorderRadius,
+    alignSelf: 'center',
+  };
+
+  const textStyles = {
+    color: btnTextColor,
+    fontSize: 16,
+    textTransform: 'uppercase',
+    textAlign: 'center',
+  };
+
+  const border = type === 'outlined' && {
+    borderColor: Colors.orange,
+    borderWidth: 2,
+  };
+
   return (
-    <TouchableOpacity onPress={() => navigation.navigate('Nyheter')}>
-      <View style={styles.button}>
-        <Text style={styles.buttonText}>{text}</Text>
+    <TouchableOpacity onPress={onPress} activeOpacity={0.7}>
+      <View style={[containerStyles, border]}>
+        <Text style={[textStyles]}>{text}</Text>
       </View>
     </TouchableOpacity>
   );
 };
 
-const styles = StyleSheet.create({
-  button: {
-    padding: 50,
-    borderRadius: 8,
-    paddingVertical: 14,
-    paddingHorizontal: 10,
-    elevation: 3,
-    backgroundColor: Colors.orange,
-  },
-  buttonText: {
-    fontSize: 16,
-    textAlign: 'center',
-    color: Colors.black,
-  },
-});
 export default Button;
