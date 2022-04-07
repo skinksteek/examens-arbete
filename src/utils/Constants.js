58372bb2a3e01e35e74a9e26317559ccbfeca2b8
skinksteek <linusigelstrom@gmail.com>
2022-04-07 09:31:58 +0200

    added image & text comp to assets

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -4,7 +4,10 @@ import WidePageScreen from '../screens/WidePageScreen';
 import CenteredScreen from '../screens/CenteredScreen';
 import PageListingScreen from '../screens/PageListingScreen';
 
-const templates = {
+import TextModule from '../components/Text';
+import ImageModule from '../components/Image';
+
+export const templates = {
   '91.484d9c7317ce4f0e1b68b3': CenteredScreen,
   '91.484d9c7317ce4f0e1b68ad': SubPageScreen,
   '91.484d9c7317ce4f0e1b68b8': EmptyScreen,
@@ -12,4 +15,9 @@ const templates = {
   '91.484d9c7317ce4f0e1b6946': PageListingScreen,
 };
 
-export default templates;
+export const components = {
+  image: ImageModule,
+  text: TextModule,
+};
+
+export default {templates, components};
