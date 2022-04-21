87654c05125821463b64baee0661e2a71d160b9e
skinksteek <linusigelstrom@gmail.com>
2022-04-21 14:59:04 +0200

    new color and styling variables

--- src/styles/Colors.js
+++ src/styles/Colors.js
@@ -3,6 +3,7 @@ export const Colors = {
   orange: '#F89221',
   gray: '#F8F9FA',
   black: '#000000',
+  fontColor: '#333333',
 };
 
 export default Colors;
--- src/styles/global.js
+++ src/styles/global.js
@@ -2,19 +2,68 @@ import {StyleSheet} from 'react-native';
 import {Colors} from './Colors';
 
 export const globalStyling = StyleSheet.create({
-  default: {
+  default_view: {
     flex: 1,
     padding: 20,
     backgroundColor: Colors.gray,
     marginBottom: 60,
   },
   default_scroll: {
-    marginBottom: 60,
-  },
-  defaultFlex: {
-    flex: 1,
-    padding: 20,
     backgroundColor: Colors.gray,
     marginBottom: 60,
+  },
+  centered: {
+    flex: 1,
+    padding: 20,
+    marginBottom: 60,
+  },
+  flex: {
+    flex: 1,
+  },
+});
+
+export const textStyling = StyleSheet.create({
+  headerL: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    margin: 5,
+    textAlign: 'center',
+    color: Colors.fontColor,
+  },
+  headerM: {
+    fontWeight: 'bold',
+    fontSize: 25,
+    margin: 5,
+    textAlign: 'center',
+    color: Colors.fontColor,
+  },
+  title: {
+    fontWeight: '500',
+    fontSize: 25,
+    padding: 10,
+    color: Colors.fontColor,
+  },
+  desc: {
+    fontSize: 16,
+    fontWeight: '500',
+    padding: 10,
+    color: Colors.fontColor,
+  },
+  publish: {
+    fontSize: 13,
+    fontWeight: '400',
+    position: 'absolute',
+    bottom: 10,
+    left: 20,
+    color: Colors.fontColor,
+  },
+  simpletext: {
+    color: Colors.fontColor,
+    flex: 1,
+    alignItems: 'center',
+    paddingLeft: 15,
+    paddingRight: 15,
+    paddingBottom: 15,
+    paddingTop: 5,
   },
 });
